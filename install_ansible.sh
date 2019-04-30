fb3820cd30b5bce3e50591b819f900168287ee70
rirze <cheedella.sourabh@gmail.com>
2019-04-30 13:28:58 -0400

    fix flags and couchdb signing key

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 
-sudo apt install python-pip
-sudo pip install git+https://github.com/ansible/ansible.git@stable-2.8
+sudo apt install python-pip -y
+sudo -H pip install git+https://github.com/ansible/ansible.git@stable-2.8
+
+ansible-playbook setup.yml -K
