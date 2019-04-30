00d8d14f19d6f03fe955ccbbb31cf377b82b4553
rirze <cheedella.sourabh@gmail.com>
2019-04-30 13:36:00 -0400

    change apt to apt-get and move apt-keys to before modifying /etc/apt/source.list

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-sudo apt install python-pip -y
+sudo apt-get install python-pip -y
 sudo -H pip install git+https://github.com/ansible/ansible.git@stable-2.8
 
 ansible-playbook setup.yml -K
